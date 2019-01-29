5b5cfee6860693c03856eabb3e87eb0a60191723
Michael Gale <m.gale@warwick.ac.uk>
2019-01-29 17:16:28 +0000

    shouldBe instead of ==

--- 5-hspec/test/Test.hs
+++ 5-hspec/test/Test.hs
@@ -22,9 +22,9 @@ prop_reverse xs = reverse (reverse xs) == xs
 -- | Run the tests when the program starts.
 main = hspec $ do
     describe "symbol" $ do
-        it "symbol 0 == ' '" (symbol 0 == ' ')
-        it "symbol 1 == 'X'" (symbol 1 == 'X')
-        it "symbol (-1) == '?'" (symbol (-1) == '?')
+        it "symbol 0 == ' '" (symbol 0 `shouldBe` ' ')
+        it "symbol 1 == 'X'" (symbol 1 `shouldBe` 'X')
+        it "symbol (-1) == '?'" (symbol (-1) `shouldBe` '?')
     describe "reverse" $ do
         prop "reverse (reverse xs) == xs"
             (\(xs :: [Int]) -> reverse (reverse xs) == xs)
